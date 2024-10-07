5029fb95c6edad32e8414725a6488e3524b2afcf
Susmitha <susmithaj833@gmail.com>
2024-10-07 18:18:08 +0530

    commited

---
+++ abc/src/entity/Customer.java
@@ -0,0 +1,13 @@
+package entity;
+import java.io.*;
+public class Customer {
+	public static void main(String[] args) {
+		int a=9;
+		int b=10;
+		System.out.println("Hello world");
+		System.out.println(a+b);
+	}
+	
+	
+
+}
