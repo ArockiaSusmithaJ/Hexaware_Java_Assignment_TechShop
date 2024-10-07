5845c0bc4b7a1f2b3a9eef07f75eaf5496dafe12
ArockiaSusmithaJ <137278080+ArockiaSusmithaJ@users.noreply.github.com>
2024-10-07 18:57:20 +0530

    Delete VirtualArtGallery directory

--- VirtualArtGallery/src/exception/GalleryNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-public class GalleryNotFoundException extends Exception {
-    private static final long serialVersionUID = 1L; // Serializable ID
-
-    public GalleryNotFoundException(String message) {
-        super(message);
-    }
-}
--- VirtualArtGallery/src/exception/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-public class UserNotFoundException extends Exception {
-    private static final long serialVersionUID = 1L; // Serializable ID
-
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
